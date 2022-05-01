e3d75bd2008443fd80ac51ccdd5c852ac8739be8
Grant Holtes <gwholtes@gmail.com>
2022-05-01 22:39:29 +1000

    added colour

--- creativeprojects/scripts/canvasFunctionsV2.js
+++ creativeprojects/scripts/canvasFunctionsV2.js
@@ -18,21 +18,39 @@ function init() {
     window.requestAnimationFrame(draw);
 }
 
-function warmup(x0, y0) {
-
-    return { "x0": x0, "y0": y0 }
+function hex2RGB(hex) {
+    let R = parseInt((hex[1] + hex[2]).toUpperCase(), 16);
+    let G = parseInt((hex[3] + hex[4]).toUpperCase(), 16);
+    let B = parseInt((hex[5] + hex[6]).toUpperCase(), 16);
+    return [R, G, B];
 }
+
+function toggleSeries() {
+    var x = document.getElementById("toggleSeries");
+    if (x.innerHTML === "Peter de Jong") {
+        x.innerHTML = "Polynomial";
+    } else {
+        x.innerHTML = "Peter de Jong";
+    }
+}
+
 
 //Render preview
 function draw() {
-    ctx.fillStyle = "rgba(0,0,0,0.1)";
-    ctx.fillRect(0, 0, width, height);
-
     //Get inputs
     let a = parseFloat(document.getElementById("inputA").value);
     let b = parseFloat(document.getElementById("inputB").value);
     let c = parseFloat(document.getElementById("inputC").value);
     let d = parseFloat(document.getElementById("inputD").value);
+    //Get colours
+    let backgroundColour = document.getElementById("colorpickerBackground").value;
+    let col1 = hex2RGB(document.getElementById("colorpickerForeground1").value);
+    var functionType = document.getElementById("toggleSeries");
+
+
+    ctx.fillStyle = backgroundColour;
+    ctx.fillRect(0, 0, width, height);
+
 
     // var imageData = ctx.createImageData(width, height);
     var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
@@ -40,37 +58,72 @@ function draw() {
     var x0 = getRandomInt(10);
     var y0 = getRandomInt(10);
 
-    //Warm up
-    for (j = 0; j < 10; j++) {
-        x1 = Math.sin(a * x0) - Math.cos(b * y0);
-        y1 = Math.sin(c * x0) - Math.cos(d * y0);
-        x0 = x1;
-        y0 = y1;
-    }
-    //Render
-    for (i = 0; i < 50000; i++) {
-        //based on http://paulbourke.net/fractals/peterdejong/
-        x1 = Math.sin(a * x0) - Math.cos(b * y0);
-        y1 = Math.sin(c * x0) - Math.cos(d * y0);
-        x0 = x1;
-        y0 = y1;
-        if (i % 5000 == 0) {
-            //Avoid solutions that only visit a small number of points
-            x0 = getRandomInt(100) / 100.0;
-            y0 = getRandomInt(100) / 100.0;
-            //Warm up
-            for (j = 0; j < 10; j++) {
-                x1 = Math.sin(a * x0) - Math.cos(b * y0);
-                y1 = Math.sin(c * x0) - Math.cos(d * y0);
-                x0 = x1;
-                y0 = y1;
-            }
+    if (functionType == "Peter de Jong") {
+        //Warm up
+        for (j = 0; j < 10; j++) {
+            x1 = Math.sin(a * x0) - Math.cos(b * y0);
+            y1 = Math.sin(c * x0) - Math.cos(d * y0);
+            x0 = x1;
+            y0 = y1;
         }
-        X = parseInt((x1 + 2.1) * (width / 4.2));
-        Y = parseInt((y1 + 2.1) * (height / 4.2));
-        imageData = setPixelColour(imageData, X, Y, width, 255, 255, 255, 255);
+        //Render
+        for (i = 0; i < 50000; i++) {
+            //based on http://paulbourke.net/fractals/peterdejong/
+            x1 = Math.sin(a * x0) - Math.cos(b * y0);
+            y1 = Math.sin(c * x0) - Math.cos(d * y0);
+            x0 = x1;
+            y0 = y1;
+            if (i % 5000 == 0) {
+                //Avoid solutions that only visit a small number of points
+                x0 = getRandomInt(100) / 100.0;
+                y0 = getRandomInt(100) / 100.0;
+                //Warm up
+                for (j = 0; j < 10; j++) {
+                    x1 = Math.sin(a * x0) - Math.cos(b * y0);
+                    y1 = Math.sin(c * x0) - Math.cos(d * y0);
+                    x0 = x1;
+                    y0 = y1;
+                }
+            }
+            X = parseInt((x1 + 2.1) * (width / 4.2));
+            Y = parseInt((y1 + 2.1) * (height / 4.2));
+            imageData = setPixelColour(imageData, X, Y, width, col1[0], col1[1], col1[2], 255);
 
+        }
+    } else {
+        //Warm up
+        for (j = 0; j < 10; j++) {
+            x1 = Math.sin(a * x0) - Math.cos(b * y0);
+            y1 = Math.sin(c * x0) - Math.cos(d * y0);
+            x0 = x1;
+            y0 = y1;
+        }
+        //Render
+        for (i = 0; i < 50000; i++) {
+            //based on http://paulbourke.net/fractals/peterdejong/
+            x1 = Math.sin(a * x0) - Math.cos(b * y0);
+            y1 = Math.sin(c * x0) - Math.cos(d * y0);
+            x0 = x1;
+            y0 = y1;
+            if (i % 5000 == 0) {
+                //Avoid solutions that only visit a small number of points
+                x0 = getRandomInt(100) / 100.0;
+                y0 = getRandomInt(100) / 100.0;
+                //Warm up
+                for (j = 0; j < 10; j++) {
+                    x1 = Math.sin(a * x0) - Math.cos(b * y0);
+                    y1 = Math.sin(c * x0) - Math.cos(d * y0);
+                    x0 = x1;
+                    y0 = y1;
+                }
+            }
+            X = parseInt((x1 + 2.1) * (width / 4.2));
+            Y = parseInt((y1 + 2.1) * (height / 4.2));
+            imageData = setPixelColour(imageData, X, Y, width, col1[0], col1[1], col1[2], 255);
+
+        }
     }
+
     ctx.putImageData(imageData, 0, 0);
 
     window.requestAnimationFrame(draw);
@@ -93,18 +146,23 @@ init(); //Start animiation loop
 
 
 //Draw once
-function drawhighRes(points = 20000000) {
+function drawhighRes(points = 5000000) {
     const canvas = document.getElementById("canvasLarge");
     const context = canvas.getContext('2d');
-    context.fillStyle = "rgba(0,0,0,1)";
-    context.fillRect(0, 0, canvas.width, canvas.width);
 
     //Get inputs
     let a = parseFloat(document.getElementById("inputA").value);
     let b = parseFloat(document.getElementById("inputB").value);
     let c = parseFloat(document.getElementById("inputC").value);
     let d = parseFloat(document.getElementById("inputD").value);
+
     let pow = parseFloat(document.getElementById("power").value);
+    let backgroundColour = document.getElementById("colorpickerBackground").value;
+    let col1 = hex2RGB(document.getElementById("colorpickerForeground1").value);
+    let col2 = hex2RGB(backgroundColour);
+
+    context.fillStyle = backgroundColour;
+    context.fillRect(0, 0, canvas.width, canvas.width);
 
     // var imageData = ctx.createImageData(width, height);
     var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
@@ -148,8 +206,6 @@ function drawhighRes(points = 20000000) {
     for (const [key, value] of Object.entries(samples)) {
         maxVal = Math.max(maxVal, value)
     }
-    let col1 = [255.0, 255.0, 255.0];
-    let col2 = [0.0, 0, 0];
     for (const [key, value] of Object.entries(samples)) {
         X = Math.floor(key / canvas.width);
         Y = key % canvas.width;
